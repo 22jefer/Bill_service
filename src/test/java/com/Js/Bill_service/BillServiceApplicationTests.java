2628dd3dea07d35d8990943b3fac62d6bc4bff7e
22jefer <jefferson02830@gmail.com>
2023-08-20 16:21:21 -0500

    Master
    * Se creo el modelo base para la elaboracion del microservicio bill
    * Bill_service se conecta correctamente con myql workbench
      Datos adicionales:
      port : 8083

---
+++ src/test/java/com/Js/Bill_service/BillServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Js.Bill_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
